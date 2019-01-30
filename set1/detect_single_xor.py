26bdcc68ef0b4dc048ae769b1b405766555a1a08
thomaskim <thomaskim@dropbox.com>
2019-01-29 20:41:39 -0800

    Add pearsons chi squared test

--- set1/detect_single_xor.py
+++ set1/detect_single_xor.py
@@ -4,9 +4,5 @@ with open('./encrypted.txt', 'r') as enc:
   res = []
 
   for i, enc_string in enumerate(enc):
-    for r in decode_cipher(enc_string.strip('\n'), 15):
-      if r.score > 70:
-        res.append(r)
-  
-  for candidates in sorted(res, key=lambda s_p: s_p.score):
-    print(candidates)
+    res.append(decode_cipher(enc_string.strip()))
+  print(min(res, key=lambda ps: ps.score))
--- set1/single_byte_xor.py
+++ set1/single_byte_xor.py
@@ -1,6 +1,6 @@
 import codecs
 from fixed_xor import fixed_xor
-from collections import namedtuple
+from collections import namedtuple, Counter
 
 character_freq = {
   'A': 8.167,
@@ -34,35 +34,35 @@ character_freq = {
 hex = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 # hex = '0e3647e8592d35514a081243582536ed3de6734059001e3f535ce6271032'
 
-def _evaluate(plaintext: str) -> float:
-  # plaintext should be a regular Python string
-  score = 0.0
-  for letter in plaintext:
-    if letter.upper() in character_freq:
-      score += character_freq[letter.upper()]
-  return score
+PlaintextScore = namedtuple('PlaintextScore', ['score', 'plaintext', 'key'])
 
-ScorePlaintext = namedtuple('ScorePlaintext', ['score', 'plaintext', 'key'])
+def _evaluate(deciphered_text: str) -> float:
+  # deciphered_text should be a regular Python string
+  # Evaluates using a Pearson chi-squared test
+  decipher_text_freq = Counter(deciphered_text.upper())
+  num_letters = len(deciphered_text)
+  for letter in decipher_text_freq:
+    decipher_text_freq[letter] /= num_letters
 
-def decode_cipher(h: str, limit=5) -> [ScorePlaintext]:
+  chi_squared = 0.0
+  for c in character_freq:
+    expected_freq = character_freq[c] / 100
+    chi_squared += ( (decipher_text_freq[c] - expected_freq) ** 2 ) / expected_freq
+
+  return chi_squared
+
+def decode_cipher(h: str) -> PlaintextScore:
   # decodes single-byte XOR cipher against English alphabet
-  # evaluates each result and sorts them by character frequency score
-  # YOU should decide which one is probably the key
+  # evaluates each result and sorts them by score
   scores = {}
   for letter in character_freq:
     cipherkey = codecs.encode(bytes(letter, 'utf-8') * (len(h) // 2), 'hex').decode()
-    plaintext = codecs.decode(fixed_xor(h, cipherkey), 'hex').decode('latin-1')
-    scores[letter] = ScorePlaintext(_evaluate(plaintext), plaintext, letter)
+    deciphered_text = codecs.decode(fixed_xor(h, cipherkey), 'hex').decode('latin-1')
+    scores[letter] = PlaintextScore(_evaluate(deciphered_text), deciphered_text, letter)
 
-  max_score_plaintext = 0
-  res = []
-  for letter in scores:
-    if scores[letter].score > max_score_plaintext:
-      max_score_plaintext = scores[letter].score
-    res.append(scores[letter])
-
-  return sorted(res, key=lambda s_p: s_p.score, reverse=True)[:limit]
+  return min([scores[c] for c in scores], key=lambda ps: ps.score)
 
 if __name__ == "__main__":
-  for choices in decode_cipher(hex, 3):
+  # I'm cooking ciphers like a pound of bacon!
+  for choices in decode_cipher(hex):
     print(choices)
